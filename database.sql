0efff3901c5b0cf51cbb74bf2e62100ce72de0e2
جين هيوك <a123@123ui-MacBookPro.local>
2025-09-26 14:42:33 +0900

    add dummy data

--- database.sql
+++ database.sql
@@ -2,13 +2,19 @@
 CREATE DATABASE IF NOT EXISTS board_system;
 USE board_system;
 
+
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users; 
+
+
 -- 사용자 테이블
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('user','admin') NOT NULL DEFAULT 'user';
+    role ENUM('user','admin') NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -19,7 +25,21 @@ CREATE TABLE posts (
     content TEXT NOT NULL,
     user_id INT NOT NULL,
     filename VARCHAR(255),
+    role ENUM('user','admin') NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+
+
+-- users 더미 데이터 2개 비번은 둘 다 'password'
+INSERT INTO users (username, email, password, role)
+VALUES
+('alice', 'alice@example.com', '$2a$12$z48Ha4BLyQFUm/ecFLiE1Ot7T4QOPW5GKRt8nEU8X32J6txRrirH.', 'user'), 
+('bob', 'bob@example.com', '$2a$12$z48Ha4BLyQFUm/ecFLiE1Ot7T4QOPW5GKRt8nEU8X32J6txRrirH.', 'admin');
+
+-- posts 더미 데이터 2개
+INSERT INTO posts (title, content, user_id, filename)
+VALUES
+('첫 번째 게시글', '이것은 첫 번째 게시글 내용입니다.', 1, 'file1.txt'),
+('관리자 게시글', '관리자가 작성한 게시글 내용입니다.', 2, 'file2.txt');
