d59c16c6f66edd702b3b968bb41a5dde8bbadcba
yccs-26 <yuchan5@uos.ac.kr>
2025-09-26 17:23:30 +0900

    update database.sql with secret column in posts table & create comments table

--- database.sql
+++ database.sql
@@ -26,10 +26,20 @@ CREATE TABLE posts (
     user_id INT NOT NULL,
     filename VARCHAR(255),
     role ENUM('user','admin') NOT NULL DEFAULT 'user',
+    secret BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 
 -- users 더미 데이터 2개 비번은 둘 다 'password'
@@ -43,3 +53,9 @@ INSERT INTO posts (title, content, user_id, filename,role)
 VALUES
 ('첫 번째 게시글', '이것은 첫 번째 게시글 내용입니다.', 1, 'file1.txt','user'),
 ('관리자 게시글', '관리자가 작성한 게시글 내용입니다.', 2, 'file2.txt','admin');
+
+INSERT INTO comments (post_id, user_id, content)
+VALUES
+(1, 1, '첫 번째 게시글에 대한 댓글입니다.'),
+(1, 2, '관리자의 댓글입니다.'),
+(2, 1, '관리자 게시글에 대한 일반 사용자 댓글입니다.');
