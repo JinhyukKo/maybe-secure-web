c426e277138399d9dbda8237107b5a6cf4ead7bc
Jinhyuk Ko <kobin1970@gmail.com>
2025-09-27 17:01:38 +0900

    add role in session

--- auth/login.php
+++ auth/login.php
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
         $file = $_SERVER['DOCUMENT_ROOT'].'/index.php';
         header("Location: /");
         exit();
--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <div>
     <?php
-        echo "Hello! ".$_SESSION['username'];
+        echo "Hello! ".$_SESSION['username']."<br/>";
+
+        echo "you are ".$_SESSION['role'];
     ?>
 </div>
